99e600b22213030d7cd53b052b555d297f3c24cd
DevCherki <154801632+DevCherki@users.noreply.github.com>
2023-12-25 14:26:38 +0100

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -2,12 +2,52 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+# ANSI escape codes for text color
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+# Your text
+text = r'''
+ ________
+< CHERKI >
+ --------
+    \
+     \
+                                   .::!!!!!!!:.
+  .!!!!!:.                        .:!!!!!!!!!!!!
+  ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$
+      :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P
+      $$$$$##WX!:      .<!!!!UW$$$$"  $$$$$$$$#
+      $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$*
+      ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R"
+        "*$bd$$$$      '*$$$$$$$$$$$o+#"
+             """"          """""""
+   _____ _               _    _    _____ __  __ _______ _____
+  / ____| |             | |  (_)  / ____|  \/  |__   __|  __ \
+ | |    | |__   ___ _ __| | ___  | (___ | \  / |  | |  | |__) |
+ | |    | '_ \ / _ \ '__| |/ / |  \___ \| |\/| |  | |  |  ___/
+ | |____| | | |  __/ |  |   <| |  ____) | |  | |  | |  | |
+  \_____|_| |_|\___|_|  |_|\_\_| |_____/|_|  |_|  |_|  |_|
+
+
+FACEBOOK:dev.cherki
+'''
+
+# Print the colored text
+colored_text = f"{Colors.WARNING}{text}{Colors.ENDC}"
+print(colored_text)
+
 # استخراج بيانات المستخدم
-email = input("البريد الإلكتروني: ")
-password = input("كلمة المرور: ")
+email = input("EMAIL: ")
+password = input("PASSWORD APP: ")
 
 # الحصول على مسار ملف نصي يحتوي على قائمة بريدية
-txt_file_path = input("أدخل مسار ملف نصي (txt) يحتوي على قائمة بريدية مفصولة بمسافات: ")
+txt_file_path = input("Enter the path to a text (txt) file containing your mailing list separated by lines: ")
 with open(txt_file_path, "r") as txt_file:
     receivers_str = txt_file.read()
 
@@ -15,12 +55,12 @@ with open(txt_file_path, "r") as txt_file:
 receivers = receivers_str.split()
 
 # الحصول على محتوى ملف HTML من المستخدم
-html_file_path = input("أدخل مسار ملف HTML: ")
+html_file_path = input("Enter the message in (html) file format: ")
 with open(html_file_path, "r") as html_file:
     html_content = html_file.read()
 
 sender = email
-subject = "تأكيد حسابك"
+subject = input("SUBJECT: ")
 
 try:
     server = smtplib.SMTP("smtp.gmail.com", 587)
